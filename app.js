64048a32c124005b936ac20dd9570ff6224f4817
Nayru <101361108+nsorroza@users.noreply.github.com>
2022-04-04 10:48:23 -1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+ var num = 10
+
+ let num1 = 100
+ num1 = 200
+
+ console.log(num1)
+
+ const num2 = 1000
+
+ console.log(num2)
+
+const persona = {
+   edad: 26,
+   nombre: 'Nicolas',
+   apellido: 'Gonzalez',
+   dni: 227377372,
+ }
+
+ const meses = ['Enero', 2344, undefined, true, persona]
+
+ console.log(meses[4].apellido);
+ const comision = [   {     nombre: 'Nicolas'   },   {     nombre: 'Carlitos'  },   {     nombre: 'Duco'   } ]
+ console.log(comision)
+
+ Condicional
+ const numero = 100; const numero2 = 100;
+ if (numero > numero2) {   console.log('Si es mayor o igual') } else {   console.log('No es mayor') }
+ Alert
+ alert('Hola Mundo'); console.log('Hola Mundo' console.warn('Hola Mundo' console.error('Error Hola Mundo' console.group( console.log('Hola Mundo' console.log('Hola Mundo' console.log('Hola Mundo') document.write('Hola Mundo') const nombreUsuario = prompt('Ingrese su nombre')console.log(nombreUsuario);
+
+const num1 = 3
+const num2 = 2
+
+const res = num1 + num2
+
+console.log(res)
+
+
+El usuario ingrese dos numeros por prompt y esos numeros se sumen, y se muestre el resultado por consola.
+
+const nombreUsuario = prompt('Ingrese su nombre')
+
+document.write('Hola ' + nombreUsuario)
+document.write(`Hola ${nombreUsuario}`)
+
+
